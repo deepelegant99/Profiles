5f882a3e228e93f044896940bf40ab95219094af
Phillip Wong <deepelegant99@gmail.com>
2021-06-12 17:45:26 -0700

    uses querySelectorAll to select for elements to style

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const body = document.body;
 body.style.backgroundColor="#6ae04c";
-console.log(body.title)
-// console.log(document.doctype)
-console.log(document.all[6])
 
 
-//Print out all the box-titles. 
+//Getting elements by classnames
 let titles = body.getElementsByClassName("box-title");
 for(let i =0; i<titles.length; i++){
     titles[i].style.color="blue";
@@ -19,10 +16,24 @@ for(let i =0; i<titles.length; i++){
 let bottons = body.getElementsByClassName("button");
 for(let i =0; i<titles.length; i++){
     bottons[i].style.backgroundColor = '#21debe';
+    bottons[i].style.fontWeight='bold';
+    bottons[i].style.color = 'orange';
 }
 
-//Print out all the descriptions
-let descriptions = body.getElementsByTagName("img");
-for ( let i =0; i < descriptions.length; i++){
-    // descriptions[i].style.display = 'none';
+//Getting elements by tag names 
+// let images = body.getElementsByTagName("img");
+// for ( let i =0; i < images.length; i++){
+//     images[i].style.display = 'none';
+// }
+
+//Getting elements by quarySelectionAll
+let paragraphs = body.querySelectorAll('p');
+for(let i=0; i<paragraphs.length; i++){
+    paragraphs[i].style.fontWeight = 'bold';
+    paragraphs[i].style.color = 'gray';
+}
+
+let projnums= body.querySelectorAll('.user-follow-info');
+for(let i=0; i<projnums.length; i++){
+    projnums[i].style.color='orange';
 }
